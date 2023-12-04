8047467581c1eb9dce7cdaff169b71de987f4b51
dtadres <david.tadres@lifesci.ucsb.edu>
2023-12-04 14:43:01 -0800

    add Ashley's stitching algorithm sh file

---
+++ scripts/stitch.sh.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=stitch_nii
+#SBATCH --partition=owners
+#SBATCH --time=2-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --output=./logs/stitchlog3.out
+#SBATCH --open-mode=append
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+# ml antspy/0.2.2
+date
+python3 -u /home/users/david/brainsss_david/scripts/stitch_nii_only.py
